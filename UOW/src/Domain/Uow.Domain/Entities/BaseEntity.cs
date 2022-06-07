24fd3ca6f3d77c5b7205c5d8607e88199b489488
Marian Spoială <marian.spoiala@gmail.com>
2022-06-07 07:55:30 +0300

    Created solution and some of the projects.

---
+++ UOW/src/Domain/Uow.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Uow.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ UOW/src/Infrastructure/Uow.Data/UowContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Uow.Data
+{
+    public class UowContext : DbContext
+    {
+        public UowContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
